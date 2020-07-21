e18575b7564308bbf34269708ed892a90a64b3d4
Adson Ileon <ileonrd@gmail.com>
2020-07-21 20:40:20 -0300

    Main modificado para funcionar com os últimos datasets gerados.

--- main3.m
+++ main3.m
@@ -2,11 +2,12 @@ clear; clc;
 % Parâmetros relacionados ao vídeo
 % root1 = 'D:\Adson\YUVSequences\NETVC(AV1)\720p\';
 root1 = 'D:\Adson\YUVSequences\';
-root2 = 'D:\Adson\Datasets\Angulares_QuatroClasses\';
-dataSetNames = {'dataset_4x4_4x32.csv', 'dataset_8x4_8x32.csv', 'dataset_16x4_16x32.csv', 'dataset_32x4_32x32.csv'};
-dataSetCount = {'count_4x4_4x32.txt', 'count_8x4_8x32.txt', 'count_16x4_16x32.txt', 'count_32x4_32x32.txt'};
-% dataSetNames = {'dataset_32x4_32x32.csv'};
-% dataSetCount = {'count_32x4_32x32.txt'};
+% root2 = 'D:\Adson\Datasets\Angulares_QuatroClasses\';
+root2 = 'D:\Adson\Datasets\Angulares_CincoClasses\';
+dataSetNames = {'dataset_4x4_4x32.csv', 'dataset_8x4_8x32.csv', 'dataset_16x4_16x32.csv', 'dataset_32x4_32x32.csv', 'dataset_64x64_64x64.csv'};
+dataSetCount = {'count_4x4_4x32.txt', 'count_8x4_8x32.txt', 'count_16x4_16x32.txt', 'count_32x4_32x32.txt', 'count_64x64_64x64.txt'};
+% dataSetNames = {'dataset_64x64_64x64.csv'};
+% dataSetCount = {'count_64x64_64x64.txt'};
 % Formato de cada linha no arquivo CSV
 fid = fopen("format3.txt", "r");
 format = fgetl(fid);
@@ -17,7 +18,7 @@ fid = fopen("columnsName3.txt","r");
 columnsName = fgets(fid);
 fclose(fid);
 
-for d=1:4
+for d=1:5
     fidcount = fopen(strcat(root2,char(dataSetCount(d))),"r");
     dataSetName = split(char(dataSetNames(d)),".");
     fidsetnew = fopen(strcat(root2,char(dataSetName(1)),"_features.csv"),"w");
@@ -29,7 +30,6 @@ for d=1:4
     fidset = fopen(strcat(root2,char(dataSetNames(d))),"r");
     fgetl(fidset); %Nome das colunas
     line = fgetl(fidcount);
-    sequence="";
     while ischar(line)
         nBlocks = str2num(char(line));
 %         blockResults = zeros(nBlocks,1233);
@@ -40,64 +40,50 @@ for d=1:4
         xbr = zeros(1,nBlocks,'uint32');
         ybr = zeros(1,nBlocks,'uint32');
         class = zeros(1,nBlocks,'uint8');
+        ids = zeros(1,nBlocks,'uint8');
+        qps = zeros(1,nBlocks,'uint8');
+        examples = strings(1,nBlocks);
         for i=1:nBlocks
-            example = fgetl(fidset);
+            examples(1,i) = fgetl(fidset);
+            if i==1
+               values = split(examples(1,i),",");
+               bw = str2num(char(values(2)));
+               bh = str2num(char(values(3)));
+               vm = uint16(512);
+               bY = vm(ones(bh+2, bw+2, nBlocks, 'uint16'));
+            end
+        end
+        wb = parwaitbar(nBlocks);
+        parfor i=1:nBlocks
+            example = examples(1,i);
             values = split(example,",");
             poc(1,i) = str2num(char(values(5)));
-            if i==1
-                if ~strcmp(sequence,char(values(1)))
-                    if ~strcmp(sequence,"")
-                        fclose(fidVideo);
-                    end
-                    sequence = char(values(1));
-                    [video,width,height,bitDepth,skip] = verifyVideo(sequence);
-                    disp(strcat(video," ",num2str(bitDepth), " ", num2str(skip)));
-                    fidVideo = fopen(strcat(root1,video),"r");
-                    if bitDepth==8
-                        bytes = 1.5*width*height;
-                    else
-                        bytes = 3*width*height;
-                    end
-                end
-                qp = str2num(char(values(4)));
-                bw = str2num(char(values(2)));
-                bh = str2num(char(values(3)));
-                vm = uint16(512);
-                bY = vm(ones(bh+2, bw+2, nBlocks, 'uint16'));
-                disp(sequence);
-                disp(num2str(qp));
-                disp(strcat(num2str(bw),"x",num2str(bh)));
-                fseek(fidVideo,poc(1,i)*skip*bytes,'bof');
-%                 status = fseek(fidVideo,poc(1,i)*skip*bytes,'bof');
-%                 if status == -1
-%                     disp("Erro ao posicionar o arquivo");
-%                 end
-                [y,~,~] = yuvRead(fidVideo,width,height,bitDepth);
-            elseif poc(1,i)~=poc(1,i-1)
-                fseek(fidVideo,poc(1,i)*skip*bytes,'bof');
-%                 status = fseek(fidVideo,poc(1,i)*skip*bytes,'bof');
-%                 if status == -1
-%                     disp("Erro ao posicionar o arquivo");
-%                 end
-                [y,~,~] = yuvRead(fidVideo,width,height,bitDepth);
+            qps(1,i) = str2num(char(values(4)));
+            sequence = char(values(1));
+            [video,width,height,bitDepth,skip,id] = verifyVideo(sequence);
+            fidVideo = fopen(strcat(root1,video),"r");
+            if bitDepth==8
+                bytes = 1.5*width*height;
+            else
+                bytes = 3*width*height;
             end
+            fseek(fidVideo,poc(1,i)*skip*bytes,'bof');
+            [y,~,~] = yuvRead(fidVideo,width,height,bitDepth);
             xtl(1,i) = str2num(char(values(6)));
             ytl(1,i) = str2num(char(values(7)));
             xbr(1,i) = str2num(char(values(8)));
             ybr(1,i) = str2num(char(values(9)));
             bY(:,:,i) = block(y,bw,bh,ytl(1,i)+1,ybr(1,i)+1,xtl(1,i)+1,xbr(1,i)+1);
             class(1,i) = uint8(char(values(16)));
-        end
-        wb = parwaitbar(nBlocks);
-        parfor i=1:nBlocks
-            blocko = bY(:,:,i);
-            s = sobel(blocko);
-            r = roberts(blocko);
-            p = prewitt(blocko);
-            m = media(blocko);
-            e = desvio_variancia(blocko);
-            blockm = medfilt2(blocko); %Bloco após filtro mediana
-            blockc = imadjust(blocko); %Bloco após contraste
+            ids(1,i) = id;
+            fclose(fidVideo);
+            s = sobel(bY(:,:,i));
+            r = roberts(bY(:,:,i));
+            p = prewitt(bY(:,:,i));
+            m = media(bY(:,:,i));
+            e = desvio_variancia(bY(:,:,i));
+            blockm = medfilt2(bY(:,:,i)); %Bloco após filtro mediana
+            blockc = imadjust(bY(:,:,i)); %Bloco após contraste
             sm = sobel(blockm);
             rm = roberts(blockm);
             pm = prewitt(blockm);
@@ -112,7 +98,7 @@ for d=1:4
             wb.progress();
         end
         for i=1:nBlocks
-            fprintf(fidsetnew,format,sequence,bw,bh,qp,poc(1,i),xtl(1,i),xbr(1,i),ytl(1,i),ybr(1,i),blockResults(i,:),char(class(1,i)));
+            fprintf(fidsetnew,format,returnVideo(ids(1,i)),bw,bh,qps(1,i),poc(1,i),xtl(1,i),xbr(1,i),ytl(1,i),ybr(1,i),blockResults(i,:),char(class(1,i)));
 %             fprintf(fidsetnew,format,sequence,bw,bh,qp,poc(i),xtl(i),xbr(i),ytl(i),ybr(i),blockResults(i,1:411),char(class(i)));
 %             fprintf(fidsetnewfm,format,sequence,bw,bh,qp,poc(i),xtl(i),xbr(i),ytl(i),ybr(i),blockResults(i,412:822),char(class(i)));
 %             fprintf(fidsetnewfc,format,sequence,bw,bh,qp,poc(i),xtl(i),xbr(i),ytl(i),ybr(i),blockResults(i,823:1233),char(class(i)));
--- verifyVideo.m
+++ verifyVideo.m
@@ -1,117 +1,136 @@
-function [video,width,height,bitdepth,skip] = verifyVideo(name)
+function [video,width,height,bitdepth,skip, id] = verifyVideo(name)
 if strcmp(name,"Dark")
     video = "Treinamento\720p\Dark\Dark_1280x720_60fps_8bit_420_120f.yuv";
     width = 1280;
     height = 720;
     bitdepth = 8;
     skip = 1;
+    id = 1;
 elseif strcmp(name,"Kristen")
     video = "Treinamento\720p\Kristen\KristenAndSara_1280x720_60fps_8bit_420_120f.yuv";
     width = 1280;
     height = 720;
     bitdepth = 8;
     skip = 1;
+    id = 2;
 elseif strcmp(name,"Netflix_DinnerScene")
     video = "Treinamento\720p\Netflix_DinnerScene\Netflix_DinnerScene_1280x720_60fps_8bit_420_120f.yuv";
     width = 1280;
     height = 720;
     bitdepth = 8;
     skip = 1;
+    id = 3;
 elseif strcmp(name,"Netflix_DrivingPOV")
     video = "Treinamento\720p\Netflix_DrivingPov\Netflix_DrivingPOV_1280x720_60fps_8bit_420_120f.yuv";
     width = 1280;
     height = 720;
     bitdepth = 8;
     skip = 1;
+    id = 4;
 elseif strcmp(name,"Vidyo4")
     video = "Treinamento\720p\Vidyo4\Vidyo4_1280x720p_60fps_8bit_420_120f.yuv";
     width = 1280;
     height = 720;
     bitdepth = 8;
     skip = 1;
+    id = 5;
 elseif strcmp(name,"Beauty")
     video = "Treinamento\1080p\Beauty\Beauty_1920x1080_120fps_420_8bit_YUV.yuv";
     width = 1920;
     height = 1080;
     bitdepth = 8;
     skip = 10;
+    id = 6;
 elseif strcmp(name,"Jockey")
     video = "Treinamento\1080p\Jockey\Jockey_1920x1080_120fps_420_8bit.yuv";
     width = 1920;
     height = 1080;
     bitdepth = 8;
     skip = 10;
+    id = 7;
 elseif strcmp(name,"Netflix_Tunnel")
     video = "Treinamento\1080p\Netflix_Tunnel\Netflix_TunnelFlag_1920x1080_60fps_8bit_420_60f.yuv";
     width = 1920;
     height = 1080;
     bitdepth = 8;
     skip = 1;
+    id = 8;
 elseif strcmp(name,"Rush")
     video = "Treinamento\1080p\Rush\rush_field_cuts_1080p_60f.yuv";
     width = 1920;
     height = 1080;
     bitdepth = 8;
     skip = 1;
+    id = 9;
 elseif strcmp(name,"Touchdown")
     video = "Treinamento\1080p\Touchdown\touchdown_pass_1080p_60f.yuv";
     width = 1920;
     height = 1080;
     bitdepth = 8;
     skip = 1;
+    id = 10;
 elseif strcmp(name,"BuildingHall2")
     video = "Treinamento\4K\BuildingHall2\BuildingHall2_3840x2160_50fps_10bit_420.yuv";
     width = 3840;
     height = 2160;
     bitdepth = 10;
     skip = 16;
+    id = 11;
 elseif strcmp(name,"Lips")
     video = "Treinamento\4K\Lips\Lips_3840x2160_120fps_10bit.yuv";
     width = 3840;
     height = 2160;
     bitdepth = 10;
     skip = 20;
+    id = 12;
 elseif strcmp(name,"Netflix_Dancers")
     video = "Treinamento\4K\Netflix_Dancers\Netflix_Dancers_4096x2160_60fps_10bit_420_60f.yuv";
     width = 4096;
     height = 2160;
     bitdepth = 10;
     skip = 2;
+    id = 13;
 elseif strcmp(name,"SunBath")
     video = "Treinamento\4K\SunBath\SunBath_3840x2160_50fps_10bit.yuv";
     width = 3840;
     height = 2160;
     bitdepth = 10;
     skip = 10;
+    id = 14;
 elseif strcmp(name,"ToddlerFountain")
     video = "Treinamento\4K\ToddlerFountain\ToddlerFountain_4096x2160_60fps_10bit_420_jvet.yuv";
     width = 4096;
     height = 2160;
     bitdepth = 10;
     skip = 10;
+    id = 15;
 elseif strcmp(name,"C")
     video = "Validacao\C\RaceHorses_832x480_30fps_8bit_420.yuv";
     width = 832;
     height = 480;
     bitdepth = 8;
     skip = 8;
+    id = 16;
 elseif strcmp(name,"D")
     video = "Validacao\D\BQSquare_416x240_60fps_8bit_420.yuv";
     width = 416;
     height = 240;
     bitdepth = 8;
     skip = 8;
+    id = 17;
 elseif strcmp(name,"E")
     video = "Validacao\E\Johnny_1280x720_60fps_8bit_420.yuv";
     width = 1280;
     height = 720;
     bitdepth = 8;
     skip = 8;
+    id = 18;
 elseif strcmp(name,"F(1)")
     video = "Validacao\F(1)\SlideEditing_1280x720_30fps_8bit_420.yuv";
     width = 1280;
     height = 720;
     bitdepth = 8;
     skip = 8;
+    id = 19;
 end
 end
